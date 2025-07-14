6e9ca776ca8d77262434cff9ec98c344dab729cd
Gustavo Antunes Veiga do Carmo <gustavo.carmo@estudante.ifto.edu.br>
2025-07-14 20:11:55 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/veiga/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/veiga/workshopmongo/WorkshopmongoApplication.java
@@ -2,8 +2,10 @@ package com.veiga.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.veiga.workshopmongo")
 public class WorkshopmongoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/veiga/workshopmongo/services/UserService.java
@@ -0,0 +1,24 @@
+package com.veiga.workshopmongo.services;
+
+import com.veiga.workshopmongo.domain.User;
+import com.veiga.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElse(null);
+    }
+
+}
