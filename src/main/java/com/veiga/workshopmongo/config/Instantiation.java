086ad424801a9576aeeb150036ba110130626f26
Gustavo Antunes Veiga do Carmo <gustavo.carmo@estudante.ifto.edu.br>
2025-07-17 17:44:36 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/veiga/workshopmongo/config/Instantiation.java
+++ src/main/java/com/veiga/workshopmongo/config/Instantiation.java
@@ -1,11 +1,14 @@
 package com.veiga.workshopmongo.config;
 
+import com.veiga.workshopmongo.domain.Post;
 import com.veiga.workshopmongo.domain.User;
+import com.veiga.workshopmongo.repository.PostRepository;
 import com.veiga.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @Configuration
@@ -14,16 +17,34 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User gustavo = new User(null, "Gustavo Antunes", "gustavo@gmail.com");
         User cristovao = new User(null, "Cristovão Junior", "cristovao@gmail.com");
         User maria = new User(null, "Maria D Carmo", "maria@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"),
+                "Partiu Viagem",
+                "Vou viajar para São Paulo. Abraços!",
+                gustavo);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"),
+                "Bom dia",
+                "Acordei feliz hoje!",
+                cristovao);
+
         userRepository.saveAll(Arrays.asList(gustavo, cristovao, maria));
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
     }
 
 }
