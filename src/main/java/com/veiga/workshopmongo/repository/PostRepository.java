595f149f00e652ce793c73918c4af62735b99500
Gustavo Antunes Veiga do Carmo <gustavo.carmo@estudante.ifto.edu.br>
2025-07-17 20:23:53 -0300

    Consulta simples com @Query

--- src/main/java/com/veiga/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/veiga/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.veiga.workshopmongo.repository;
 
 import com.veiga.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,9 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
 
 }
